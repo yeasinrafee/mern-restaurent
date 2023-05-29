5ffc2da4abb4e2154a5bdf9dc30a25fb114f1649
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-29 17:08:02 +0600

    Menu page added, parallox added on menu cover

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,22 @@
 import { Helmet } from "react-helmet-async";
 
+import Cover from "../../Sharec/Cover/Cover";
+
+import coverImg from "./../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <h2>menu</h2>
+      <Cover img={coverImg} title="Our Menu" />
+      <PopularMenu />
+      <Cover img={coverImg} title="Our Menu" />
+      <PopularMenu />
+      <Cover img={coverImg} title="Our Menu" />
+      <PopularMenu />
     </div>
   );
 };
