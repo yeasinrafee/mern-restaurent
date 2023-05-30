e2a2085e9c90f78e20e7869e70f24bc1e0e028d6
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-30 16:58:14 +0600

    Food card ready for Order page

---
+++ src/pages/Order/Order/Order.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Cover from "../../Sharec/Cover/Cover";
+import orderCover from "./../../../assets/shop/banner2.jpg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../hooks/useMenu";
+const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <Cover img={orderCover} title={"Order Food"} />
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Dessert</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Order;
